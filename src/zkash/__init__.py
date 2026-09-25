1aaee604a3f8270b61c38d43fd1c2a8021887206
mikky1sCp <alexei.nk2012@gmail.com>
2026-09-25 21:49:16 +0700

    Update __init__.py

--- src/zkash/__init__.py
+++ src/zkash/__init__.py
@@ -1,5 +1,5 @@
-from .model import Zkash10K, Zkash01M, build_model
+from .model import Zkash10K, Zkash01M, Zkash1M, build_model
 from .utils import count_params
 
-__version__ = "2.0.0"
-__all__ = ["Zkash10K", "Zkash01M", "build_model", "count_params"]
+__version__ = "3.0.0"
+__all__ = ["Zkash10K", "Zkash01M", "Zkash1M", "build_model", "count_params"]
