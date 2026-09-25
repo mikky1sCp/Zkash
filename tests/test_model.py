30290a07bec83ef985c984b1433217d5d4a6d6a6
mikky1sCp <alexei.nk2012@gmail.com>
2026-09-25 21:07:31 +0700

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,20 +1,23 @@
 import torch
-from zkash.model import Zkash10K
+from zkash.model import Zkash10K, Zkash01M
 from zkash.utils import count_params
 
 
-def test_param_count():
+def test_param_count_10k():
+    assert count_params(Zkash10K()) == 10_000
+
+
+def test_param_count_01m():
+    assert count_params(Zkash01M()) == 100_000
+
+
+def test_forward_shape_10k():
     m = Zkash10K()
-    assert count_params(m) == 10_000
-
-
-def test_forward_shape():
-    m = Zkash10K(n_in=64, n_out=8)
-    x = torch.randn(4, 64)
-    y = m(x)
+    y = m(torch.randn(4, 64))
     assert y.shape == (4, 8)
 
 
-def test_no_bias_in_output():
-    m = Zkash10K()
-    assert m.fc3.bias is None
+def test_forward_shape_01m():
+    m = Zkash01M()
+    y = m(torch.randn(4, 64))
+    assert y.shape == (4, 8)
