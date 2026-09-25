3815fd5ab4966cae86b233ce19be10922ab97314
mikky1sCp <alexei.nk2012@gmail.com>
2026-09-25 21:51:58 +0700

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")/.."
 PYTHONWARNINGS="ignore::FutureWarning" \
 PYTHONPATH=src python -m zkash.evaluate \
-  --config configs/zkash_01m.yaml \
-  --ckpt checkpoints/zkash01m.pt
+  --config configs/zkash_1m.yaml \
+  --ckpt checkpoints/zkash1m.pt
