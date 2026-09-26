892e6a4a476bb6aa4ca352884a782ab542e0468d
mikky1sCp <alexei.nk2012@gmail.com>
2026-09-26 14:59:24 +0700

    Delete src/zkash directory

--- src/zkash/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model import Zkash10K, Zkash01M, Zkash1M, build_model
-from .utils import count_params
-
-__version__ = "3.0.0"
-__all__ = ["Zkash10K", "Zkash01M", "Zkash1M", "build_model", "count_params"]
