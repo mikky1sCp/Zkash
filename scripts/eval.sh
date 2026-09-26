cc2df9890ba3e1df23df2f09a526f99bbd912397
mikky1sCp <alexei.nk2012@gmail.com>
2026-09-26 14:59:17 +0700

    Delete scripts directory

--- scripts/eval.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")/.."
-PYTHONWARNINGS="ignore::FutureWarning" \
-PYTHONPATH=src python -m zkash.evaluate \
-  --config configs/zkash_1m.yaml \
-  --ckpt checkpoints/zkash1m.pt
